0cc339c1d34122cb3dd261e2e2dcbde31128e812
BrunocSena <114001990+BrunocSena@users.noreply.github.com>
2023-08-27 14:34:14 -0300

    TALVEZ?

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: `https://backend-bebidas.vercel.app/`
+    baseURL: `https://backend-bebidas-bt8vjxc73-brunocsena.vercel.app/`
 });
